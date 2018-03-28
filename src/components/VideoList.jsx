0db396fad35265ce984f987578703c903a4afd66
Stephen Choe <stphnchoe@gmail.com>
2018-03-28 02:46:31 -0700

    Cleans up currently unecessary files and videolist, videolistitem components render

---
+++ src/components/VideoList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import VideoListItem from './VideoListItem';
+
+const VideoList = props => (
+  <div>
+    <VideoListItem />
+  </div>
+);
+
+export default VideoList;
---
+++ src/components/VideoListItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const VideoListItem = () => (
+  <div>
+    <h1>Title</h1>
+    <h2>Picture</h2>
+    <h3>Broadcaster</h3>
+    <h3>Views</h3>
+    <h5>Follow</h5>
+    <h3>Category</h3>
+  </div>
+);
+
+export default VideoListItem;
