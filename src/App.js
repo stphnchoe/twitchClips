b9d38c72e88e1b36bc88592864e8aeb511b9cceb
Stephen Choe <stphnchoe@gmail.com>
2018-03-27 23:49:16 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class LambdaDemo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {loading: false, msg: null};
+  }
+
+  handleClick = (e) => {
+    e.preventDefault();
+
+    this.setState({loading: true});
+    fetch('/.netlify/functions/hello')
+      .then(response => response.json())
+      .then(json => this.setState({loading: false, msg: json.msg}));
+  }
+
+  render() {
+    const {loading, msg} = this.state;
+
+    return <p>
+      <button onClick={this.handleClick}>{loading ? 'Loading...' : 'Call Lambda'}</button><br/>
+      <span>{msg}</span>
+    </p>
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <LambdaDemo/>
+      </div>
+    );
+  }
+}
+
+export default App;
