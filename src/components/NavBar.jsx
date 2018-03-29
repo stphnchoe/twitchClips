3e752fd9961a4d0271be0960b41bfcfba629baa9
Stephen Choe <stphnchoe@gmail.com>
2018-03-29 00:23:16 -0700

    Fixes browse and home buttons on navbar to route on click

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
-import React, { Component } from 'react'
-import { Menu, Input } from 'semantic-ui-react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Menu, Input } from 'semantic-ui-react';
 
 class NavBar extends Component {
   state = { activeItem: 'home' }
@@ -11,11 +12,11 @@ class NavBar extends Component {
 
     return (
       <Menu inverted >
-        <Menu.Item name='home' onClick={this.handleItemClick} icon='home' />
+        <Menu.Item name='home' as={Link} to='/' icon='home' />
         <img src="https://cdn1.iconfinder.com/data/icons/simple-icons/256/twitch-256-black.png" alt="twitch logo" height="50px"/>
         <span className="navbar-clips-header">Twitch Clips</span>
         <Menu.Item />
-        <Menu.Item name='Browse' onClick={this.handleItemClick} />
+        <Menu.Item name='Browse' onClick={this.handleItemClick} as={Link} to='/browse' />
         <Menu.Menu position='right'>
           <Menu.Item>
             <Input icon='search' placeholder='Search...' />
