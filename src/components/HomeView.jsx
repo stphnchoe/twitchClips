f7c27908a38f38ef299620cb1ffc9140c96154ec
Stephen Choe <stphnchoe@gmail.com>
2018-03-31 14:50:30 -0700

    Changes video title font on home and clip view page and adds a title to homeview page

--- src/components/HomeView.jsx
+++ src/components/HomeView.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Icon } from 'semantic-ui-react';
+import { Grid, Header, Icon } from 'semantic-ui-react';
 import ClipViewer from './ClipViewer';
 import moment from 'moment';
 
@@ -7,6 +7,10 @@ const HomeView = ({ video }) => (
   <Grid container stretched centered>
     <Grid.Column columns={2} computer={10} tablet={16} mobile={16}>
       <Grid.Row computer={16} tablet={16} mobile={16}>
+        <Header id="homeview-title" as='h1' color="white">
+          <Icon name="star" color="yellow" size="large" />
+          Top Trending Clip Today
+        </Header>
         <ClipViewer video={video} />
         <Grid.Column>
           <div className="white-font">
